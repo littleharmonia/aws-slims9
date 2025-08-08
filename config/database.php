2e6cd6b25404cd4636a2937cf5db1e974396fd3f
root <root@ip-172-31-37-143.ap-southeast-3.compute.internal>
2025-08-08 13:07:13 +0000

    Menambahkan folder template dll

---
+++ config/database.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    /**
+     * Default database node profile
+     */
+    'default_profile' => 'SLiMS',
+
+    /**
+     * SLiMS as Service, One SLiMS for many library
+     * ----------------------------------------------------
+     * 
+     * Switching database node access based on rule,
+     * such as domain, ip, port etc
+     * 
+     * How to :
+     * 1. make file with name database_proxy.php in config/
+     * 2. make your own rule in that file.
+     * 3. change this value to true
+     */
+    'proxy' => false,
+
+    /**
+     * Nodes profile
+     */
+    'nodes' => [
+        'SLiMS' => [
+            'host' => 'localhost',
+            'database' => 'slims9',
+            'port' => 3306,
+            'username' => 'root',
+            'password' => 'Passwordnya123*',
+            'options' => [
+                'storage_engine' => 'MyISAM'
+            ]
+        ]
+    ]
+];
